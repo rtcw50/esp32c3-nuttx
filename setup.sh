0f7c30a7b41fb86303cba00d51fdeba002ab1394
John Russo <johnhrusso@gmail.com>
2026-05-30 09:01:45 -0500

    C3 program integration, apps example helloc3 is working. Still some issues with the makefile for C3 programs

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@
 export PATH=/opt/risc-v/bin:$HOME/.espressif/tools/python/v6.0/venv/bin:$PATH
 # For Zen-C Projects
 export ZC_ROOT=$HOME/Documents/Projects/zen-c/zenc
+# For C3 Projects
+export C3C_LIB=/opt/c3/c3/lib
