3dc336bebd128d96650b7e625af2c1ef046059f2
John Russo <johnhrusso@gmail.com>
2026-09-21 19:14:03 -0500

    Fix make_apps_patch.sh to actually capture the lvgl diffs. LVGL is copied as a package file by nuttx apps and not git controlled.

--- make_apps_patch.sh
+++ make_apps_patch.sh
@@ -4,4 +4,12 @@ pushd apps
 git diff -p > ../apps.patch
 popd
 
+mkdir -p /tmp/lvgl
+cp apps/graphics/lvgl/v9.2.1.zip /tmp/lvgl
+pushd /tmp/lvgl
+unzip -o v9.2.1.zip
+popd
+diff -r --exclude=*.o --text  apps/graphics/lvgl/lvgl  /tmp/lvgl/lvgl-9.2.1 >> apps.patch
+rm -rf /tmp/lvgl
+
 
